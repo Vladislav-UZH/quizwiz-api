c643ea5d675e000431c433a94c0e498ef0d2ece4
toniname <drednoyta1@ukr.net>
2024-11-03 22:53:54 +0200

    init commit

--- quizgame/src/main/java/com/example/kahoot/dto/CreateQuestionDTO.java
+++ quizgame/src/main/java/com/example/kahoot/dto/CreateQuestionDTO.java
@@ -7,4 +7,20 @@ public class CreateQuestionDTO {
     private List<CreateAnswerDTO> answers;
 
     // Геттеры и сеттеры
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<CreateAnswerDTO> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<CreateAnswerDTO> answers) {
+        this.answers = answers;
+    }
 }
