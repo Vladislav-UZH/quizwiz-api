186a73ebf7eeeab6db25e2a0d75ad977fa142d71
random-words <153684721+random-words@users.noreply.github.com>
2024-12-10 12:49:47 +0200

    check postman endpoints

--- src/main/resources/db/migration/V1__Create_Quiz_Schema.sql
+++ src/main/resources/db/migration/V1__Create_Quiz_Schema.sql
@@ -8,28 +8,28 @@ CREATE TABLE users (
 -- Створення таблиці quiz_stack
 -- Створення таблиці quiz
 CREATE TABLE quiz (
-                      id SERIAL PRIMARY KEY,
-                      title VARCHAR(255) NOT NULL
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
 );
 
 -- Створення таблиці question
 -- Кожне питання прив’язане до конкретного quiz через quiz_id
 CREATE TABLE question (
-                          id SERIAL PRIMARY KEY,
-                          quiz_id INTEGER NOT NULL,
-                          text VARCHAR(255) NOT NULL,
-                          CONSTRAINT fk_question_quiz FOREIGN KEY (quiz_id)
-                              REFERENCES quiz (id)
-                              ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    quiz_id INTEGER NOT NULL,
+    text VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_question_quiz FOREIGN KEY (quiz_id)
+            REFERENCES quiz (id)
+            ON DELETE CASCADE
 );
 
 -- Створення таблиці options
 -- Кожен варіант відповіді прив’язаний до питання через question_id
 CREATE TABLE options (
-                         id SERIAL PRIMARY KEY,
-                         question_id INTEGER NOT NULL,
-                         text VARCHAR(255) NOT NULL,
-                         CONSTRAINT fk_options_question FOREIGN KEY (question_id)
-                             REFERENCES question (id)
-                             ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    text VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_options_question FOREIGN KEY (question_id)
+        REFERENCES question (id)
+        ON DELETE CASCADE
 );
