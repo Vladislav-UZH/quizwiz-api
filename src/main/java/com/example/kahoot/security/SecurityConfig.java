43c387c9f86eff5310b8974adb3f6fc42284e408
Vladislav-UZH <perebziak.vladislav@gmail.com>
2025-02-04 14:00:57 +0200

    merge

--- src/main/java/com/example/kahoot/security/SecurityConfig.java
+++ src/main/java/com/example/kahoot/security/SecurityConfig.java
@@ -19,8 +19,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
-
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 
